f98c06aca09d11922c58afedf7567c21b0e3eee1
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-11-26 21:25:56 +0200

    2

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const accordionFaq = new Accordion('.accordion-container');
+const accordionFaq = new Accordion('.accordion-container', { duration: 1200 });
 
 export default accordionFaq;
