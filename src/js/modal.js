1dd31e5d490b0e63084b823f37ce78d57dd08456
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-11-27 11:43:31 +0200

    3

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,27 +1,34 @@
 document.getElementById('openModalBtn').addEventListener('click', function () {
-  document.getElementById('modal').style.display = 'block';
-  document.body.classList.add('no-scroll');
+  const modal = document.getElementById('modal');
+  modal.classList.add('show'); // Додаємо клас для показу модалки
+  document.body.classList.add('no-scroll'); // Вимикаємо прокручування тіла
 });
 
+// Закриття модалки при натисканні на кнопку
 document.getElementById('closeModalBtn').addEventListener('click', function () {
-  document.getElementById('modal').style.display = 'none';
-  document.body.classList.remove('no-scroll');
+  const modal = document.getElementById('modal');
+  modal.classList.remove('show'); // Видаляємо клас для приховування модалки
+  document.body.classList.remove('no-scroll'); // Вмикаємо прокручування тіла
 });
 
+// Закриття модалки при натисканні на посилання в меню
 const menuLinks = document.querySelectorAll('.header-li');
 menuLinks.forEach(function (link) {
   link.addEventListener('click', function () {
-    document.getElementById('modal').style.display = 'none';
-    document.body.classList.remove('no-scroll');
+    const modal = document.getElementById('modal');
+    modal.classList.remove('show'); // Видаляємо клас для приховування модалки
+    document.body.classList.remove('no-scroll'); // Вмикаємо прокручування тіла
   });
 });
 
+// Закриття модалки при натисканні поза її межами
 document.addEventListener('click', function (event) {
   const modal = document.getElementById('modal');
   const modalContent = document.querySelector('#modal .modal-content');
 
+  // Перевірка, чи натиснуто поза модалкою
   if (event.target === modal) {
-    modal.style.display = 'none';
-    document.body.classList.remove('no-scroll');
+    modal.classList.remove('show'); // Приховуємо модалку
+    document.body.classList.remove('no-scroll'); // Вмикаємо прокручування тіла
   }
 });
